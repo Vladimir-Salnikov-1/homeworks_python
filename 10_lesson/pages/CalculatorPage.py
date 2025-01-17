0cd7cac4efe39677e99505177d3e88a78fcfeb90
unknown <ya-vladimir-salnikov.ru>
2025-01-18 01:41:04 +0300

    3 добавил Allure к калькулятору (в класс и тест)

--- 10_lesson/pages/CalculatorPage.py
+++ 10_lesson/pages/CalculatorPage.py
@@ -12,63 +12,83 @@ class CalculatorPage:
         self._browser.maximize_window()
 
     # Открываем калькулятор, ждем появления необходимых элементов:
+    @allure.step("Открыть калькулятор")
     def open_calculator(self):
         """Этот метод открывает калькулятор и ждет появления
         необходимых элементов."""
-        self._browser.get(
-            "https://bonigarcia.dev/selenium-webdriver-java/\
-                slow-calculator.html")
-        WebDriverWait(self._browser, 10).until(
-            EC.presence_of_element_located(
-                (By.CSS_SELECTOR, ".col-sm-12.py-10"))
-        )
+        with allure.step("Открыть сайт"):
+            self._browser.get(
+                "https://bonigarcia.dev/selenium-webdriver-" +
+                "java/slow-calculator.html")
+
+        with allure.step("Дождаться прогрузки страницы"):
+            WebDriverWait(self._browser, 10).until(
+                EC.presence_of_element_located(
+                    (By.CSS_SELECTOR, ".col-sm-12.py-10"))
+            )
 
     # Вводим значение в поле ввода секунд ожидания, фиксируем значение:
+    @allure.step("Ввести в форму секунды, в данном случае: {second} сек.")
     def fill_form(self, second: int) -> str:
         """Этот метод принимает на вход параметр "секунды",
         вводит это значение в поле ввода секунд ожидания,
         фиксирует значение."""
-        input = self._browser.find_element(By.CSS_SELECTOR, "#delay")
-        input.clear()
-        input.send_keys(second)
-        input_text = int(self._browser.find_element(
-            By.CSS_SELECTOR, "#delay").get_attribute("value"))
+        with allure.step("Находим поле ввода для времени ожидания"):
+            input = self._browser.find_element(By.CSS_SELECTOR, "#delay")
+        with allure.step("Очищаем поле ввода"):
+            input.clear()
+        with allure.step("Вводим необходимое значение"):
+            input.send_keys(second)
+        with allure.step("Берем значение атрибута value из формы ввода"):
+            input_text = int(self._browser.find_element(
+                By.CSS_SELECTOR, "#delay").get_attribute("value"))
         return input_text
 
     # Нажимаем кнопку 7:
+    @allure.step("Нажать кнопку 7")
     def click_button_7(self):
         """Этот метод нажимает на калькуляторе кнопку "7"."""
         ls = self._browser.find_elements(By.CSS_SELECTOR, ".btn")
         ls[1].click()
 
     # Нажимаем кнопку +:
+    @allure.step("Нажать кнопку +")
     def click_button_plus(self):
         """Этот метод нажимает на калькуляторе кнопку "+"."""
         ls = self._browser.find_elements(By.CSS_SELECTOR, ".btn")
         ls[4].click()
 
     # Нажимаем кнопку 8:
+    @allure.step("Нажать кнопку 8")
     def click_button_8(self):
         """Этот метод нажимает на калькуляторе кнопку "8"."""
         ls = self._browser.find_elements(By.CSS_SELECTOR, ".btn")
         ls[2].click()
 
     # Нажимаем кнопку =, фиксируем время ожидания ответа:
+    @allure.step("Нажать кнопку =")
     def click_button_equal(self) -> int:
         """Этот метод нажимает на калькуляторе кнопку "=" и фиксирует
         время ожидания"""
-        ls = self._browser.find_elements(By.CSS_SELECTOR, ".btn")
-        ls[15].click()
-        start_time = time.time()
-        waiter = WebDriverWait(self._browser, 55, 0.1)
-        waiter.until(
-            EC.text_to_be_present_in_element(
-                (By.CSS_SELECTOR, ".screen"), "15"))
-        end_time = time.time()
-        waiting_time = int(end_time - start_time)
+        with allure.step("Нажать на ="):
+            ls = self._browser.find_elements(By.CSS_SELECTOR, ".btn")
+            ls[15].click()
+        with allure.step("Фиксируем время"):
+            with allure.step("запускаем таймер"):
+                start_time = time.time()
+            with allure.step("Ждем пока в окошке не появится результат"):
+                waiter = WebDriverWait(self._browser, 55, 0.1)
+                waiter.until(
+                    EC.text_to_be_present_in_element(
+                        (By.CSS_SELECTOR, ".screen"), "15"))
+            with allure.step("Останавливаем время"):
+                end_time = time.time()
+            with allure.step("берем значение из нашего секундомера"):
+                waiting_time = int(end_time - start_time)
         return waiting_time
 
     # Получаем и фиксируем ответ:
+    @allure.step("Получить ответ из окошка калькулятора")
     def get_result(self) -> str:
         """Этот метод берет значение из окошка калькулятора и
         переводит его в текстовое значение"""
--- 10_lesson/test_calculator.py
+++ 10_lesson/test_calculator.py
@@ -2,12 +2,18 @@ from pages.CalculatorPage import CalculatorPage
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service as ChromeService
+import allure
 
 
-def test_result_15_after_45_seconds():
-
-    browser = webdriver.Chrome(
-        service=ChromeService(ChromeDriverManager().install()))
+@allure.epic("Тестирование калькулятора")
+@allure.feature("Выдержка результата определенное время")
+@allure.title("результат 15 за 3 секунды")
+@allure.description("В результате теста должно вывестись на экран калькулятора\
+    результат операции ровно за время прописанное в спец. окошке")
+def test_result_15_after_3_seconds():
+    with allure.step("Инициализируем хром драйвер"):
+        browser = webdriver.Chrome(
+            service=ChromeService(ChromeDriverManager().install()))
 
     calculator_page = CalculatorPage(browser)
 
@@ -19,9 +25,11 @@ def test_result_15_after_45_seconds():
     waiting_time = calculator_page.click_button_equal()
     result_in_window = calculator_page.get_result()
 
-    assert waiting_time == input_text, f"Ошибка. Введенное время: {
-        input_text}, время ожидания: {waiting_time}"
-    assert result_in_window == "15", f"Ожидалось 15 а в ответе - {
-        result_in_window}"
+    with allure.step("Проверяем что введенное время = времени ожидания"):
+        assert waiting_time == input_text, f"Ошибка. Введенное время: {
+            input_text}, время ожидания: {waiting_time}"
+    with allure.step("Проверяем что результат в окне калькулятора = 15"):
+        assert result_in_window == "15", f"Ожидалось 15 а в ответе - {
+            result_in_window}"
 
     browser.quit()
